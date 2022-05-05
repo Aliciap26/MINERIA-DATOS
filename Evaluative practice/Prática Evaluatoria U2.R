d6da6065039c8b07bcd25d526a65941875d16b4d
AliciaPérez <44456885+Aliciap26@users.noreply.github.com>
2022-05-04 21:17:21 -0700

    Add files via upload

---
+++ Evaluative practice/Prática Evaluatoria U2.R
@@ -0,0 +1,39 @@
+#Howard Erwin
+#Perez Alicia
+#We assign the graphics library, and open the file that we will analyze called Project data.
+
+library(ggplot2)
+
+#Read doc "Project-Data"
+movies <- read.csv(file.choose())
+movies
+
+#Filters 
+#we filter the information (data) that we want to obtain, in this case the genre and the production studies.
+
+movies<-movies[movies$Studio=="Buena Vista Studios"|movies$Studio=="Fox"|movies$Studio=="Paramount Pictures"|movies$Studio=="Sony"|movies$Studio=="Universal"|movies$Studio=="WB",]
+
+movies<-movies[movies$Genre=="action"|movies$Genre=="adventure"|movies$Genre=="animation"|movies$Genre=="comedy"|movies$Genre=="drama",]
+
+
+# Graphs
+#We designate the value x the gender data.
+#Y percentage
+
+Ggraph <- ggplot(movies, aes(x=Genre, y=Gross...US))
+
+Ggraph + geom_jitter(aes(size=Budget...mill.,color=Studio)) + geom_boxplot(size=0.4, alpha=0.8)
+
+GGgraph <- Ggraph + geom_jitter(aes(size=Budget...mill.,color=Studio)) + geom_boxplot(size=0.4, alpha=0.8)
+
+GGgraph
+
+# Graphs/colors date
+#We define the color of the points and the size.
+GGgraph +
+  xlab("Genre") +
+  ylab("Groos % US") +
+  ggtitle("Domestic Gross % by Genre")+
+  theme(axis.title.x = element_text(color = "Purple", size=14),
+        axis.title.y = element_text(color = "Purple", size=14),
+        plot.title = element_text(color = "Black", size=18,hjust=0.5))
